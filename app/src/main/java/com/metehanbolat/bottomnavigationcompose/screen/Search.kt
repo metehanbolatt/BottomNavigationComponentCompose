c027ac6e50c9342926c9a923e210c59585a1a000
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-08-02 00:10:23 +0300

    Screens was added.

---
+++ app/src/main/java/com/metehanbolat/bottomnavigationcompose/screen/Search.kt
@@ -0,0 +1,21 @@
+package com.metehanbolat.bottomnavigationcompose.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SearchScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Search", fontSize = 30.sp)
+    }
+}
